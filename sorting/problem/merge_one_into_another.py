6dceb4b3f02943646e8931c8f4db6d65c6671c75
Siddhant <siddhant@onlinedegree.iitm.ac.in>
2022-07-25 00:13:03 +0530

    added merge two sorted array

---
+++ sorting/problem/merge_one_into_another.py
@@ -0,0 +1,36 @@
+def merge_one_into_another(first, second):
+    """
+    Args:
+     first(list_int32)
+     second(list_int32)
+    Returns:
+     list_int32
+    """
+    second_copy = second[:len(first)]
+    i = 0
+    j = 0
+    k = 0
+    while i < len(first) and j < len(second_copy):
+        if first[i] <= second_copy[j]:
+            second[k] = first[i]
+            i+=1
+            k+=1
+        else:
+            second[k] = second_copy[j]
+            j+=1
+            k+=1
+
+    if i < len(first):
+        while i < len(first):
+            second[k] = first[i]
+            i+=1
+            k+=1
+            
+
+    return second
+
+
+
+if __name__ == "__main__":
+    result = merge_one_into_another([6,8,10], [4,5,7,0,0,0])
+    print(result)
